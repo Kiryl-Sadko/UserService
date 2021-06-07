8fbb77eb6d466270b61c14a37e5d4ff14c198a16
Kiryl Sadko <kirill.beltech@gmail.com>
2021-06-07 22:14:26 +0300

    persistence module has been tasted

---
+++ persistence/src/test/resources/init.sql
@@ -0,0 +1,34 @@
+-- -----------------------------------------------------
+-- Schema user_db
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `user_db`;
+USE `user_db` ;
+
+-- -----------------------------------------------------
+-- Table `user_db`.`role`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `user_db`.`role`
+(
+    `id`   BIGINT(100) NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(10) NOT NULL,
+    PRIMARY KEY (`id`)
+);
+
+-- -----------------------------------------------------
+-- Table `user_db`.`user`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `user_db`.`user`
+(
+    `id`           BIGINT(100)  NOT NULL AUTO_INCREMENT,
+    `user_name`    VARCHAR(30)  NOT NULL,
+    `password`     VARCHAR(100) NOT NULL,
+    `first_name`   VARCHAR(30)  NOT NULL,
+    `last_name`    VARCHAR(30)  NOT NULL,
+    `role`         VARCHAR(10)  NOT NULL,
+    `is_active`    TINYINT      NOT NULL DEFAULT 1,
+    `created_date` TIMESTAMP    NOT NULL,
+    PRIMARY KEY (`id`),
+    CONSTRAINT `fk_user_role`
+        FOREIGN KEY (`id`)
+            REFERENCES `user_db`.`role` (`id`)
+);
