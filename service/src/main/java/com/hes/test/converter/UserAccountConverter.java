878f81187a2bae6a35d70c47c0cc4c7a1815869c
Kiryl Sadko <kirill.beltech@gmail.com>
2021-06-08 14:54:01 +0300

    Entity to Dto converter has been created

---
+++ service/src/main/java/com/hes/test/converter/UserAccountConverter.java
@@ -0,0 +1,7 @@
+package com.hes.test.converter;
+
+import com.hes.test.dto.UserAccountDto;
+import com.hes.test.entity.UserAccount;
+
+public interface UserAccountConverter extends Converter<UserAccount, UserAccountDto> {
+}
